1c0910484516fce57819d5518de01d1a739e9895
Shayan Golafshani <shayangolafsh@gmail.com>
2021-06-03 22:58:03 -0600

    Refactor with helper functions in util.js

--- src/Hydration.js
+++ src/Hydration.js
@@ -1,4 +1,4 @@
-import averager from './util';
+import {averager,  findDataByDate,  finder}  from './util';
 
 class Hydration {
   constructor(hydrationData) {
@@ -9,15 +9,24 @@ class Hydration {
     return averager(perDayUserHydration, `numOunces`);
   }
   calculateDailyOunces(id, date) {
-    let findOuncesByDate = this.hydrationData.find((data) => id === data.userID && date === data.date);
-    return findOuncesByDate.numOunces;
+    //let findOuncesByDate = this.hydrationData.find((data) => id === data.userID && date === data.date);
+    //console.log(finder(this.hydrationData, id, date).numOunces)
+    return finder(this.hydrationData, id, date).numOunces;
+    //return findOuncesByDate.numOunces;
   }
+
+  //Please write a function that returns a string for each piece of data in the 
+  //  array that you're iterating over. It should be able to refactor, calcFirstWeekOunces,
+  // calculateRandomWeekOunces(date, id, userRepo)  
   calculateFirstWeekOunces(userRepo, id) {
     return userRepo.getFirstWeek(id, this.hydrationData).map((data) => `${data.date}: ${data.numOunces}`);
   }
   calculateRandomWeekOunces(date, id, userRepo) {
-    return userRepo.getWeekFromDate(date, id, this.hydrationData).map((data) => `${data.date}: ${data.numOunces}`);
+    //return userRepo.getWeekFromDate(date, id, this.hydrationData).map((data) => `${data.date}: ${data.numOunces}`);
+    //console.log(findDataByDate(this.hydrationData, date, id, userRepo, 'numOunces'));
+    return findDataByDate(this.hydrationData, date, id, userRepo, 'numOunces');
   }
+
 }
 
 
