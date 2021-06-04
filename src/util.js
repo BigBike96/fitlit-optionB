8b1d7ba9dfda5b3b0da76d57836b81abe070955e
Shayan Golafshani <shayangolafsh@gmail.com>
2021-06-03 22:54:46 -0600

    Add findDataByDate function with Andrew

--- src/util.js
+++ src/util.js
@@ -1,9 +1,15 @@
 export const averager = (array, key) => array.reduce((acc, data) => acc += data[key], 0) / array.length;
+
 export const finder = (array, id, date) => {
   return array.find(data => {
     return id === data.userID && date === data.date;
   });
 }
+
+export const findDataByDate = (array, date, id, userRepo, key, weekDate) => {
+  return userRepo.getWeekFromDate(date, id, array).map((data) => `${data.date}: ${data[key]}`);
+}
+// export const 
 
 //export default {averager, finder};
 
