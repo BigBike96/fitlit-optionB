46fa3b850509893f0d26a701794126973de60a4c
Shayan Golafshani <shayangolafsh@gmail.com>
2021-06-04 21:33:10 -0600

    Start post method

--- src/webAPI.js
+++ src/webAPI.js
@@ -3,7 +3,7 @@
 
 let getData = (endpoint) => fetch(`http://localhost:3001/api/v1/${endpoint}`)
   .then(response => response.json())
-  .catch(err => console.log('error'));
+  .catch(err => console.log('error', err));
 // let userData = () => fetch("http://localhost:3001/api/v1/users")
 //   .then(response => response.json())
 //   .catch(err => console.log('error'));
@@ -20,7 +20,40 @@ let getData = (endpoint) => fetch(`http://localhost:3001/api/v1/${endpoint}`)
 function retrieveData() {
   return Promise.all([getData('users'), getData('hydration'), getData('sleep'), getData('activity')])
 }
-export default {retrieveData};
+
+//start writing the post requests!
+
+/*
+http://localhost:3001/api/v1/sleep {"userID": integer, "date": string, "hoursSlept": integer, "sleepQuality": integer}
+
+*/
+
+// we need to pass in the array.length-1 as a the userId. We need to take in the date as a string. And have two
+//inputs for the hoursSlept, as the sleepQuality......
+
+
+
+let exampleObj = {"userID": integer, "date": string, "hoursSlept": integer, "sleepQuality": integer}
+let sleep1 = {"userID": 777, "date": "March/09/2020", "hoursSlept": 7, "sleepQuality": 1.5}
+
+let sendData = (sentData, url) => {
+  fetch(url, {
+    method: 'POST',
+    body: JSON.stringify(sentData), // remember how HTTP can only send and receive strings, just like localStorage?
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  })
+    .then(response => response.json())
+    .then(json => console.log(json.status))
+    .catch(err => console.log(err));
+}
+
+let postData = () => {
+  return Promise.all([sendData(sleep1, 'http://localhost:3001/api/v1/sleep')])
+}
+
+export default {retrieveData, postData};
 
 
 
