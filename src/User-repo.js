9b0e792cb560df920911017e52058379c79b9e61
Jim Charnesky <mijnesky@hotmail.com>
2021-06-06 15:12:05 -0600

    Use averager to rank user id

--- src/User-repo.js
+++ src/User-repo.js
@@ -57,41 +57,13 @@ class UserRepo {
     }, {});
   }
 
-  // rankUserIDsbyRelevantDataValue(dataSet, date, relevantData, listFromMethod) {
-  //   let sortedObjectKeys = this.isolateUsernameAndRelevantData(dataSet, date, relevantData, listFromMethod)
-  //   return Object.keys(sortedObjectKeys).sort((b, a) => {
-  //
-  //     // return averager(sortedObjectKeys[a], 'sleepQualityValue')
-  //
-  //
-  //     return (sortedObjectKeys[a].reduce((sumSoFar, sleepQualityValue) => {
-  //       sumSoFar += sleepQualityValue
-  //       return sumSoFar;
-  //     }, 0) / sortedObjectKeys[a].length)
-  //
-  //     -
-  //     // return averager(sortedObjectKeys[b], 'sleepQualityValue')
-  //     console.log(sortedObjectKeys[b]);
-  //     (sortedObjectKeys[b].reduce((sumSoFar, sleepQualityValue) => {
-  //       sumSoFar += sleepQualityValue
-  //       return sumSoFar;
-  //     }, 0) / sortedObjectKeys[b].length)
-  //
-  //   });
-  // }
-
   rankUserIDsbyRelevantDataValue(dataSet, date, relevantData, listFromMethod) {
     let sortedObjectKeys = this.isolateUsernameAndRelevantData(dataSet, date, relevantData, listFromMethod)
     return Object.keys(sortedObjectKeys).sort((b, a) => {
-      return (sortedObjectKeys[a].reduce((sumSoFar, sleepQualityValue) => {
-        sumSoFar += sleepQualityValue
-        return sumSoFar;
-      }, 0) / sortedObjectKeys[a].length) - (sortedObjectKeys[b].reduce((sumSoFar, sleepQualityValue) => {
-        sumSoFar += sleepQualityValue
-        return sumSoFar;
-      }, 0) / sortedObjectKeys[b].length)
+        return averager(sortedObjectKeys[a]) - averager(sortedObjectKeys[b])
     });
   }
+
 
   combineRankedUserIDsAndAveragedData(dataSet, date, relevantData, listFromMethod) {
     let sortedObjectKeys = this.isolateUsernameAndRelevantData(dataSet, date, relevantData, listFromMethod)
