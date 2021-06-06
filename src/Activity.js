b42f888253551c21f0bb083e4849352eea8f2d8d
Jim Charnesky <mijnesky@hotmail.com>
2021-06-06 17:10:49 -0600

    Add spaces between methods

--- src/Activity.js
+++ src/Activity.js
@@ -4,18 +4,22 @@ class Activity {
   constructor(activityData) {
     this.activityData = activityData
   }
+
   getMilesFromStepsByDate(id, date, userRepo) {
     let userStepsByDate = this.activityData.find(data => id === data.userID && date === data.date);
     return parseFloat(((userStepsByDate.numSteps * userRepo.strideLength) / 5280).toFixed(1));
   }
+
   getActiveMinutesByDate(id, date) {
     return finder(this.activityData, id, date).minutesActive
   }
+
   calculateActiveAverageForWeek(id, date, userRepo) {
     return parseFloat((userRepo.getWeekFromDate(date, id, this.activityData).reduce((acc, elem) => {
       return acc += elem.minutesActive;
     }, 0) / 7).toFixed(1));
   }
+
   accomplishStepGoal(id, date, userRepo) {
     let userStepsByDate = this.activityData.find(data => id === data.userID && date === data.date);
     if (userStepsByDate.numSteps === userRepo.dailyStepGoal) {
@@ -23,9 +27,11 @@ class Activity {
     }
     return false
   }
+
   getDaysGoalExceeded(id, userRepo) {
     return this.activityData.filter(data => id === data.userID && data.numSteps > userRepo.dailyStepGoal).map(data => data.date);
   }
+
   getStairRecord(id) {
     return this.activityData.filter(data => id === data.userID).reduce((acc, elem) => (elem.flightsOfStairs > acc) ? elem.flightsOfStairs : acc, 0);
   }
@@ -40,10 +46,12 @@ class Activity {
 
      // return parseFloat((selectedDayData.reduce((acc, elem) => acc += elem[relevantData], 0) / selectedDayData.length).toFixed(1));
   }
+
   userDataForToday(id, date, userRepo, relevantData) {
     let userData = userRepo.getDataFromUserID(id, this.activityData);
     return userData.find(data => data.date === date)[relevantData];
   }
+
   userDataForWeek(id, date, userRepo, releventData) {
     return userRepo.getWeekFromDate(date, id, this.activityData).map((data) => `${data.date}: ${data[releventData]}`);
   }
