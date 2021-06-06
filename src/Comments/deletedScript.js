95e46de284a1bfe311e4e9bcc843c7bde5976832
Andrew Vallejo <andrew.a.vallejo@gmail.com>
2021-06-06 14:39:03 -0600

    Add temporary file that has deleted script code

---
+++ src/Comments/deletedScript.js
@@ -0,0 +1,10 @@
+/*
+// makeSleepQualityHTML is never used anywhere.
+function makeSleepQualityHTML(id, sleepInfo, userStorage, method) {
+  return method.map(sleepQualityData => `<li class="historical-list-listItem">On ${sleepQualityData}/5 quality of sleep</li>`).join('');
+}
+
+// This is never used. Confirmed by dean that its not even in the rubric
+avStepGoalCard.innerText = The average daily step goal is ${userStorage.calculateAverageStepGoal()};
+*/
+
