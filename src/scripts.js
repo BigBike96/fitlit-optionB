13bed38af1cee0f8e549e05cb755a1a7e81ce855
Andrew Vallejo <andrew.a.vallejo@gmail.com>
2021-06-06 22:18:49 -0600

    Refactor hydration dom output

--- src/scripts.js
+++ src/scripts.js
@@ -4,12 +4,6 @@ import './images/the-rock.jpg'
 import './images/person-walking-on-path.jpg'
 import apiCalls from './webAPI';
 
-// hard data
-// import userData from './data/users';
-// import hydrationData from './data/hydration';
-// import sleepData from './data/sleep';
-// import activityData from './data/activity';
-
 // classes
 import User from './classes/User';
 import Activity from './classes/Activity';
@@ -19,34 +13,18 @@ import UserRepo from './classes//User-repo';
 
 // querySelectors
 const sidebarName = document.getElementById('sidebarName');
-
-
-
 const stepGoalCard = document.getElementById('stepGoalCard');
-
 const headerText = document.getElementById('headerText');
-
 const userAddress = document.getElementById('userAddress');
-
 const userEmail = document.getElementById('userEmail');
-
 const userStridelength = document.getElementById('userStridelength');
-
-
-
 const friendList = document.getElementById('friendList');
-
 const historicalWeek = document.querySelectorAll('.historicalWeek');
 const friendChallengeListToday = document.getElementById('friendChallengeListToday');
-
 const friendChallengeListHistory = document.getElementById('friendChallengeListHistory');
-
 const bigWinner = document.getElementById('bigWinner');
-
 const streakList = document.getElementById('streakList');
-
 const streakListMinutes = document.getElementById('streakListMinutes')
-
 const hydrationTodayCard = document.querySelector('#hydrationTodayCard');
 const hydrationHistoryCard = document.querySelector('#hydrationHistoryCard');
 const activityTodayCard = document.querySelector('#activityTodayCard');
@@ -61,27 +39,25 @@ window.onload = () => {
       let userData = promise[0].userData.map((user) => new User(user))
       let userRepo = new UserRepo(userData)
       let hydrationData = new Hydration(promise[1].hydrationData)
-      let sleepData = new Sleep(promise[2].sleepData).sleepData
+      let sleepData = new Sleep(promise[2].sleepData)
       let activityData = new Activity (promise[3].activityData)
       startApp(userData, userRepo, hydrationData, sleepData, activityData);
     }) 
-
 }
 
-function startApp(userData, userRepo, hydration, sleepData, activityData) {
+function startApp(userData, userRepo, hydration, sleep, activityData) {
+  let sleepData = sleep.sleepData
   let hydrationData = hydration.hydrationData
   let currentUser = findRandomUser(getRandomNum(userData), userRepo);
   let currentUserID = currentUser.id
   let currentDate = findCurrentDate(userRepo, currentUser, hydrationData)[0].date;
   let randomDate = getRandomDate(findCurrentDate(userRepo, currentUser, hydrationData));
   addInfoToSidebar(currentUser, userRepo);
-  addHydrationInfo(currentUserID, hydration, currentDate, userRepo, randomDate);
-
-  // addSleepInfo(currentUser, sleepData, currentUser, userRepo, randomDate);
-  //   let winnerNow = makeWinnerID(activityRepo, userNow, today, userRepo);
-
-//   addActivityInfo(currentUser, activityRepo, today, userRepo, randomDate, userNow, winnerNow);
-//   addFriendGameInfo(currentUser, activityRepo, userRepo, today, randomDate, userNow);
+  addInfo(currentUserID, hydration, currentDate, userRepo, randomDate);
+  // addInfo(currentUserID, sleepData, currentDate, userRepo, randomDate);
+  // let winnerNow = makeWinnerID(activityRepo, userNow, today, userRepo);
+  // addActivityInfo(currentUser, activityRepo, today, userRepo, randomDate, userNow, winnerNow);
+  // addFriendGameInfo(currentUser, activityRepo, userRepo, today, randomDate, userNow);
 }
 
 function getRandomNum(input) {
@@ -89,10 +65,7 @@ function getRandomNum(input) {
 }
 
 function findRandomUser(id, users) {
-
   return users.getDataFromID(id);
-
-
 }
 
 function findCurrentDate(users, currentUser, dataSet) {
@@ -119,84 +92,81 @@ function makeFriendHTML(user, userStorage) {
 }
 
 function makeHydrationHTML(id, hydrationInfo, userStorage, method) {
-
   return method.map(drinkData => `<li class="historical-list-listItem">On ${drinkData}oz</li>`).join('');
 }
 
 function makeSleepHTML(id, sleepInfo, userStorage, method) {
-
   return method.map(sleepData => `<li class="historical-list-listItem">On ${sleepData} hours</li>`).join('');
 }
 
 function makeSleepQualityHTML(id, sleepInfo, userStorage, method) {
-
   return method.map(sleepQualityData => `<li class="historical-list-listItem">On ${sleepQualityData}/5 quality of sleep</li>`).join('');
 }
 
 function makeStepsHTML(id, activityInfo, userStorage, method) {
-
   return method.map(activityData => `<li class="historical-list-listItem">On ${activityData} steps</li>`).join('');
 }
 
 function makeStairsHTML(id, activityInfo, userStorage, method) {
-
   return method.map(data => `<li class="historical-list-listItem">On ${data} flights</li>`).join('');
 }
 
 function makeMinutesHTML(id, activityInfo, userStorage, method) {
-
   return method.map(data => `<li class="historical-list-listItem">On ${data} minutes</li>`).join('');
 }
 
 function makeFriendChallengeHTML(id, activityInfo, userStorage, method) {
-
   return method.map(friendChallengeData => `<li class="historical-list-listItem">Your friend ${friendChallengeData} average steps.</li>`).join('');
 }
 
 function makeStepStreakHTML(id, activityInfo, userStorage, method) {
-
   return method.map(streakData => `<li class="historical-list-listItem">${streakData}!</li>`).join('');
 }
 
 
 // dom related functions
 function addInfoToSidebar(user, userStorage) {
-
   sidebarName.innerText = user.name;
-
   headerText.innerText = `${user.getFirstName()}'s Activity Tracker`;
   stepGoalCard.innerText = `Your daily step goal is ${user.dailyStepGoal}.`
   userAddress.innerText = user.address;
   userEmail.innerText = user.email;
   userStridelength.innerText = `Your stridelength is ${user.strideLength} meters.`;
-
-
-
   friendList.insertAdjacentHTML('afterBegin', makeFriendHTML(user, userStorage))
 }
 
-function addHydrationInfo(id, hydrationData, dateString, userStorage, laterDateString) {
-  const currentData = hydrationData[id]
-  console.log(currentData)
-  hydrationTodayCard.insertAdjacentHTML('afterBegin', 
+function addInfo(id, dataSet, dateString, userStorage, laterDateString) {
+
+  let data = dataSet.constructor.name
+  let todayCard = eval(`${data.toLowerCase()}TodayCard`)
+  // let card = eval(`${data}HistoryCard`)
+  let makeHtml = eval(`make${data}HTML`)
+  // console.log(card)
+  // todayCard === 'hydrationTodayCard' ? todayCard = hydrationTodayCard : 
+  //   todayCard === 'activityTodayCard' ? todayCard = activityTodayCard :
+  //     todayCard === sleepTodayCard
+
+  
+  todayCard.insertAdjacentHTML('afterBegin', 
     `<article class="card hydration-card">
   <p>You drank</p>
-  <p><span class="number">${hydrationData.calculateDailyOunces(id, dateString)}</span></p>
+  <p><span class="number">${dataSet.calculateDailyOunces(id, dateString)}</span></p>
   <p>oz water today.</p>
 </article>
 <article class="card hydration-card">
   <p>Your average water intake is</p>
-  <p><span class="number">${hydrationData.calculateAverageOunces(id)}</span></p>
+  <p><span class="number">${dataSet.calculateAverageOunces(id)}</span></p>
   <p>oz per day.</p>
-</article>`); hydrationHistoryCard.insertAdjacentHTML('afterBegin', `<article
+</article>`);
+  todayCard.insertAdjacentHTML('afterBegin', `<article
   class="card hydration-card">
   <p>Water intake this week:</p>
-  <ul class="card-list" id="hydrationThisWeek"> ${makeHydrationHTML(id, hydrationData, userStorage,
-    hydrationData.calculateFirstWeekOunces(userStorage, id))} </ul>
+  <ul class="card-list" id="hydrationThisWeek"> ${makeHtml(id, dataSet, userStorage,
+    dataSet.calculateFirstWeekOunces(userStorage, id))} </ul>
 </article>
 <article class="card hydration-card">
-  <ul class="card-list" id="hydrationEarlierWeek"> ${makeHydrationHTML(id, hydrationData,
-    userStorage, hydrationData.calculateRandomWeekOunces(laterDateString, id, userStorage))} </ul>
+  <ul class="card-list" id="hydrationEarlierWeek"> ${makeHtml(id, dataSet,
+    userStorage, dataSet.calculateRandomWeekOunces(laterDateString, id, userStorage))} </ul>
 </article>`
   );
 }
