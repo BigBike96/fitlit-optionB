b22a47c3f876b6583186eedb2138ab4868f47d63
Jim Charnesky <mijnesky@hotmail.com>
2021-06-07 19:51:39 -0600

    Move test data to seperate file

--- test/user-test.js
+++ test/user-test.js
@@ -2,47 +2,17 @@ import { expect } from 'chai';
 
 import UserRepo from '../src/User-repo';
 import User from '../src/User';
+import  userTestData  from './sampleData'
 
 describe('User', () => {
   let user1, user2, user3, user4, users;
 
   beforeEach(() => {
-    user1 = new User({
-      id: 1,
-      name: "Alex Roth",
-      address: "1234 Turing Street, Denver CO 80301-1697",
-      email: "alex.roth1@hotmail.com",
-      strideLength: 4.3,
-      dailyStepGoal: 10000,
-      friends: [2, 3, 4]
-    });
-    user2 = new User({
-      id: 2,
-      name: "Allie McCarthy",
-      address: "1235 Turing Street, Denver CO 80301-1697",
-      email: "allie.mcc1@hotmail.com",
-      strideLength: 3.3,
-      dailyStepGoal: 9000,
-      friends: [1, 3, 4]
-    });
-    user3 = new User({
-      id: 3,
-      name: "The Rock",
-      address: "1236 Awesome Street, Denver CO 80301-1697",
-      email: "therock@hotmail.com",
-      strideLength: 10,
-      dailyStepGoal: 60000,
-      friends: [1, 2, 4]
-    });
-    user4 = new User({
-      id: 4,
-      name: "Rainbow Dash",
-      address: "1237 Equestria Street, Denver CO 80301-1697",
-      email: "rainbowD1@hotmail.com",
-      strideLength: 3.8,
-      dailyStepGoal: 7000,
-      friends: [1, 2, 3]
-    });
+    console.log(userTestData);
+    user1 = new User(userTestData[0]);
+    user2 = new User(userTestData[1]);
+    user3 = new User(userTestData[2]);
+    user4 = new User(userTestData[3]);
 
     users = [user1, user2, user3, user4];
   });
@@ -93,7 +63,7 @@ describe('User', () => {
   });
 
   it('should have an array of friend id numbers', () => {
-    console.log(user3.friends);
+
     expect(user3.friends).to.deep.equal([1, 2, 4]);
   });
 
@@ -110,7 +80,6 @@ describe('User', () => {
   it('should return list of friend names from user repository', () => {
     const userRepo = new UserRepo(users);
 
-    console.log(user2.getFriendsNames(userRepo));
     expect(user2.getFriendsNames(userRepo)).to.deep.equal(['Alex Roth', 'The Rock', 'Rainbow Dash']);
   });
 });
