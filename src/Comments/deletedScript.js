01999cafcf5952607a612862d8522f5afe5c8c66
Andrew Vallejo <andrew.a.vallejo@gmail.com>
2021-06-08 15:21:32 -0600

    Add unused query selectors

--- src/Comments/deletedScript.js
+++ src/Comments/deletedScript.js
@@ -48,105 +48,105 @@ function addInfo(id, dataSet, dateString, userStorage, laterDateString) {
 
 // ============== Deans code
 
-function addHydrationInfo(id, hydrationInfo, dateString, userStorage, laterDateString) {
-  hydrationTodayCard.insertAdjacentHTML('afterBegin', `
-  <article class="card hydration-card">
-    <p>You drank</p>
-    <p>
-    <span class="number">
-    ${hydrationInfo.calculateDailyOunces(id, dateString)}
-    </span>
-    </p>
-    <p>oz water today.</p>
-  </article>
-  <article class="card hydration-card">
-    <p>Your average water intake is</p>
-    <p><span class="number">
-    <p>oz per day.</p>
-    ${hydrationInfo.calculateAverageOunces(id)}
-    </span></p> 
-  </article>`);
-  hydrationHistoryCard.insertAdjacentHTML('afterBegin', `<article class="card hydration-card">
-    <p>Water intake this week:</p>
-    <ul class="card-list" id="hydrationThisWeek">
-      ${makeHydrationHTML(id, hydrationInfo, userStorage, hydrationInfo.calculateFirstWeekOunces(userStorage, id))}
-    </ul>
-  </article>
-  <article class="card hydration-card">
-    <ul class="card-list" id="hydrationEarlierWeek">
-      ${makeHydrationHTML(id, hydrationInfo, userStorage, hydrationInfo.calculateRandomWeekOunces(laterDateString, id, userStorage))}
-    </ul>
-  </article>`);
-}
+// function addHydrationInfo(id, hydrationInfo, dateString, userStorage, laterDateString) {
+//   hydrationTodayCard.insertAdjacentHTML('afterBegin', `
+//   <article class="card hydration-card">
+//     <p>You drank</p>
+//     <p>
+//     <span class="number">
+//     ${hydrationInfo.calculateDailyOunces(id, dateString)}
+//     </span>
+//     </p>
+//     <p>oz water today.</p>
+//   </article>
+//   <article class="card hydration-card">
+//     <p>Your average water intake is</p>
+//     <p><span class="number">
+//     <p>oz per day.</p>
+//     ${hydrationInfo.calculateAverageOunces(id)}
+//     </span></p> 
+//   </article>`);
+//   hydrationHistoryCard.insertAdjacentHTML('afterBegin', `<article class="card hydration-card">
+//     <p>Water intake this week:</p>
+//     <ul class="card-list" id="hydrationThisWeek">
+//       ${makeHydrationHTML(id, hydrationInfo, userStorage, hydrationInfo.calculateFirstWeekOunces(userStorage, id))}
+//     </ul>
+//   </article>
+//   <article class="card hydration-card">
+//     <ul class="card-list" id="hydrationEarlierWeek">
+//       ${makeHydrationHTML(id, hydrationInfo, userStorage, hydrationInfo.calculateRandomWeekOunces(laterDateString, id, userStorage))}
+//     </ul>
+//   </article>`);
+// }
 
-function addSleepInfo(id, sleepInfo, dateString, userStorage, laterDateString) {
-  sleepTodayCard.insertAdjacentHTML('afterBegin', `<article class="card sleep-card">
-    <p>You slept</p> <p><span class="number">${sleepInfo.calculateDailySleep(id, dateString)}</span></p> <p>hours today.</p>
-  </article>
-  <article class="card sleep-card">
-    <p>Your sleep quality was</p> <p><span class="number">${sleepInfo.calculateDailySleepQuality(id, dateString)}</span></p><p>out of 5.</p>
-  </article>
-  <article class="card sleep-card">
-    <p>The average user's sleep quality is</p> <p><span class="number">${Math.round(sleepInfo.calculateAllUserSleepQuality() * 100) / 100}</span></p><p>out of 5.</p>
-  </article>`);
-  sleepHistoryCard.insertAdjacentHTML('afterBegin', `<article class="card sleep-card">
-    <p>Hours of sleep this week</p>
-    <ul class="card-list" id="sleepThisWeek">
-    ${makeSleepHTML(id, sleepInfo, userStorage, sleepInfo.calculateWeekSleep(dateString, id, userStorage))}
-    </ul>
-  </article>
-  <article class="card sleep-card">
-    <ul class="card-list" id="sleepEarlierWeek">
-      ${makeSleepHTML(id, sleepInfo, userStorage, sleepInfo.calculateWeekSleep(laterDateString, id, userStorage))}
-    </ul>
-  </article>`);
-}
+// function addSleepInfo(id, sleepInfo, dateString, userStorage, laterDateString) {
+//   sleepTodayCard.insertAdjacentHTML('afterBegin', `<article class="card sleep-card">
+//     <p>You slept</p> <p><span class="number">${sleepInfo.calculateDailySleep(id, dateString)}</span></p> <p>hours today.</p>
+//   </article>
+//   <article class="card sleep-card">
+//     <p>Your sleep quality was</p> <p><span class="number">${sleepInfo.calculateDailySleepQuality(id, dateString)}</span></p><p>out of 5.</p>
+//   </article>
+//   <article class="card sleep-card">
+//     <p>The average user's sleep quality is</p> <p><span class="number">${Math.round(sleepInfo.calculateAllUserSleepQuality() * 100) / 100}</span></p><p>out of 5.</p>
+//   </article>`);
+//   sleepHistoryCard.insertAdjacentHTML('afterBegin', `<article class="card sleep-card">
+//     <p>Hours of sleep this week</p>
+//     <ul class="card-list" id="sleepThisWeek">
+//     ${makeSleepHTML(id, sleepInfo, userStorage, sleepInfo.calculateWeekSleep(dateString, id, userStorage))}
+//     </ul>
+//   </article>
+//   <article class="card sleep-card">
+//     <ul class="card-list" id="sleepEarlierWeek">
+//       ${makeSleepHTML(id, sleepInfo, userStorage, sleepInfo.calculateWeekSleep(laterDateString, id, userStorage))}
+//     </ul>
+//   </article>`);
+// }
 
 
-function addActivityInfo(id, activityInfo, dateString, userStorage, laterDateString, user, winnerId) {
-  activityTodayCard.insertAdjacentHTML('afterBegin', `<article class="card activity-card">
-    <p>Step Count:</p><p>You</p><p><span class="number">${activityInfo.userDataForToday(id, dateString, userStorage, 'numSteps')}</span></p>
-  </article>
-  <article class="card activity-card">
-    <p>Step Count:</p><p>All Users</p><p><span class="number">${activityInfo.getAllUserAverageForDay(dateString, userStorage, 'numSteps')}</span></p>
-  </article>
-  <article class="card activity-card">
-    <p>Stair Count:</p><p>You</><p><span class="number">${activityInfo.userDataForToday(id, dateString, userStorage, 'flightsOfStairs')}</span></p>
-  </article>
-  <article class="card activity-card">
-    <p>Stair Count: </p><p>All Users</p><p><span class="number">${activityInfo.getAllUserAverageForDay(dateString, userStorage, 'flightsOfStairs')}</span></p>
-  </article>
-  <article class="card activity-card">
-    <p>Active Minutes:</p><p>You</p><p><span class="number">${activityInfo.userDataForToday(id, dateString, userStorage, 'minutesActive')}</span></p>
-  </article>
-  <article class="card activity-card">
-    <p>Active Minutes:</p><p>All Users</p><p><span class="number">${activityInfo.getAllUserAverageForDay(dateString, userStorage, 'minutesActive')}</span></p>
-  </article>`);
-  activityHistoryCard.insertAdjacentHTML('afterBegin', `<article class="card activity-card">
-    <p>Your steps this week</p>
-    <ul class="card-list" id="userStepsThisWeek">
-      ${makeStepsHTML(id, activityInfo, userStorage, activityInfo.userDataForWeek(id, dateString, userStorage, "numSteps"))}
-    </ul>
-  </article>
-  <article class="card activity-card">
-    <p>Your stair count this week</p>
-    <ul class="card-list" id="userStairsThisWeek">
-      ${makeStairsHTML(id, activityInfo, userStorage, activityInfo.userDataForWeek(id, dateString, userStorage, "flightsOfStairs"))}
-    </ul>
-  </article>
-  <article class="card activity-card">
-    <p>Your minutes of activity this week</p>
-    <ul class="card-list" id="userMinutesThisWeek">
-      ${makeMinutesHTML(id, activityInfo, userStorage, activityInfo.userDataForWeek(id, dateString, userStorage, "minutesActive"))}
-    </ul>
-  </article>
-  <article class="card activity-card">
-    <p>Winner's steps this week</p>
-    <ul class="card-list" id="bestUserSteps">
-      ${makeStepsHTML(user, activityInfo, userStorage, activityInfo.userDataForWeek(winnerId, dateString, userStorage, "numSteps"))}
-    </ul>
-  </article>`);
-}
+// function addActivityInfo(id, activityInfo, dateString, userStorage, laterDateString, user, winnerId) {
+//   activityTodayCard.insertAdjacentHTML('afterBegin', `<article class="card activity-card">
+//     <p>Step Count:</p><p>You</p><p><span class="number">${activityInfo.userDataForToday(id, dateString, userStorage, 'numSteps')}</span></p>
+//   </article>
+//   <article class="card activity-card">
+//     <p>Step Count:</p><p>All Users</p><p><span class="number">${activityInfo.getAllUserAverageForDay(dateString, userStorage, 'numSteps')}</span></p>
+//   </article>
+//   <article class="card activity-card">
+//     <p>Stair Count:</p><p>You</><p><span class="number">${activityInfo.userDataForToday(id, dateString, userStorage, 'flightsOfStairs')}</span></p>
+//   </article>
+//   <article class="card activity-card">
+//     <p>Stair Count: </p><p>All Users</p><p><span class="number">${activityInfo.getAllUserAverageForDay(dateString, userStorage, 'flightsOfStairs')}</span></p>
+//   </article>
+//   <article class="card activity-card">
+//     <p>Active Minutes:</p><p>You</p><p><span class="number">${activityInfo.userDataForToday(id, dateString, userStorage, 'minutesActive')}</span></p>
+//   </article>
+//   <article class="card activity-card">
+//     <p>Active Minutes:</p><p>All Users</p><p><span class="number">${activityInfo.getAllUserAverageForDay(dateString, userStorage, 'minutesActive')}</span></p>
+//   </article>`);
+//   activityHistoryCard.insertAdjacentHTML('afterBegin', `<article class="card activity-card">
+//     <p>Your steps this week</p>
+//     <ul class="card-list" id="userStepsThisWeek">
+//       ${makeStepsHTML(id, activityInfo, userStorage, activityInfo.userDataForWeek(id, dateString, userStorage, "numSteps"))}
+//     </ul>
+//   </article>
+//   <article class="card activity-card">
+//     <p>Your stair count this week</p>
+//     <ul class="card-list" id="userStairsThisWeek">
+//       ${makeStairsHTML(id, activityInfo, userStorage, activityInfo.userDataForWeek(id, dateString, userStorage, "flightsOfStairs"))}
+//     </ul>
+//   </article>
+//   <article class="card activity-card">
+//     <p>Your minutes of activity this week</p>
+//     <ul class="card-list" id="userMinutesThisWeek">
+//       ${makeMinutesHTML(id, activityInfo, userStorage, activityInfo.userDataForWeek(id, dateString, userStorage, "minutesActive"))}
+//     </ul>
+//   </article>
+//   <article class="card activity-card">
+//     <p>Winner's steps this week</p>
+//     <ul class="card-list" id="bestUserSteps">
+//       ${makeStepsHTML(user, activityInfo, userStorage, activityInfo.userDataForWeek(winnerId, dateString, userStorage, "numSteps"))}
+//     </ul>
+//   </article>`);
+// }
 
 
 
@@ -173,3 +173,52 @@ function addActivityInfo(id, activityInfo, dateString, userStorage, laterDateStr
 //     <ul class="card-list" id="hydrationEarlierWeek"> ${makeHtml(id, dataSet, userStorage, dataSet.calculateRandomWeekOunces(laterDateString, id, userStorage))} </ul>
 //   </article>`
 // );
+
+
+// <article class="card activity-card">
+// <p>Step Count:</p><p>All Users</p><p><span class="number">${activitygetAllUserAverageForDay(dateString, userStorage, 'numSteps')}</span></p>
+// </article>
+// <article class="card activity-card">
+// <p>Stair Count:</p><p>You</><p><span class="number">${activityuserDataForToday(id, dateString, userStorage, 'flightsOfStairs')}</span></p>
+// </article>
+// <article class="card activity-card">
+// <p>Stair Count: </p><p>All Users</p><p><span class="number">${activitygetAllUserAverageForDay(dateString, userStorage, 'flightsOfStairs')}</span></p>
+// </article>
+// <article class="card activity-card">
+// <p>Active Minutes:</p><p>You</p><p><span class="number">${activityuserDataForToday(id, dateString, userStorage, 'minutesActive')}</span></p>
+// </article>
+// <article class="card activity-card">
+// <p>Active Minutes:</p><p>All Users</p><p><span class="number">${activitygetAllUserAverageForDay(dateString, userStorage, 'minutesActive')}</span></p>
+// </article>`);
+// historyCard.insertAdjacentHTML('afterBegin', `<article class="card activity-card">
+// <p>Your steps this week</p>
+// <ul class="card-list" id="userStepsThisWeek">
+//   ${makeStepsHTML(id, activityInfo, userStorage, activityuserDataForWeek(id, dateString, userStorage, "numSteps"))}
+// </ul>
+// </article>
+// <article class="card activity-card">
+// <p>Your stair count this week</p>
+// <ul class="card-list" id="userStairsThisWeek">
+//   ${makeStairsHTML(id, activityInfo, userStorage, activityuserDataForWeek(id, dateString, userStorage, "flightsOfStairs"))}
+// </ul>
+// </article>
+// <article class="card activity-card">
+// <p>Your minutes of activity this week</p>
+// <ul class="card-list" id="userMinutesThisWeek">
+//   ${makeMinutesHTML(id, activityInfo, userStorage, activityuserDataForWeek(id, dateString, userStorage, "minutesActive"))}
+// </ul>
+// </article>
+// <article class="card activity-card">
+// <p>Winner's steps this week</p>
+// <ul class="card-list" id="bestUserSteps">
+//   ${makeStepsHTML(user, activityInfo, userStorage, activityuserDataForWeek(winnerId, dateString, userStorage, "numSteps"))}
+// </ul>
+// </article>`)
+
+
+// Unused querySelector 
+// const sleepTodayCard = document.querySelector('#sleepTodayCard');
+// const sleepHistoryCard = document.querySelector('#sleepHistoryCard');
+// const hydrationTodayCard = document.querySelector('#hydrationTodayCard');
+// const hydrationHistoryCard = document.querySelector('#hydrationHistoryCard');
+// const historicalWeek = document.querySelectorAll('.historicalWeek');
