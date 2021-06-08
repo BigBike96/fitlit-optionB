2d4006d777683c344db1b3aae71f022f32f73f13
Andrew Vallejo <andrew.a.vallejo@gmail.com>
2021-06-08 17:54:24 -0600

    Merge Acitivty class

--- src/classes/Activity.js
+++ src/classes/Activity.js
@@ -1,69 +1,62 @@
-import { finder, averager } from "./util";
+import { finder, averager, findDataByDate } from "./util";
 
 class Activity {
   constructor(activityData) {
     this.activityData = activityData
   }
+
   getMilesFromStepsByDate(id, date, userRepo) {
-    let userStepsByDate = this.activityData.find(data => id === data.userID && date === data.date);
+    let userStepsByDate = finder(this.activityData, id, date)
     return parseFloat(((userStepsByDate.numSteps * userRepo.strideLength) / 5280).toFixed(1));
   }
+
   getActiveMinutesByDate(id, date) {
     return finder(this.activityData, id, date).minutesActive
   }
+
   calculateActiveAverageForWeek(id, date, userRepo) {
     return parseFloat((userRepo.getWeekFromDate(date, id, this.activityData).reduce((acc, elem) => {
       return acc += elem.minutesActive;
     }, 0) / 7).toFixed(1));
   }
+
   accomplishStepGoal(id, date, userRepo) {
-    let userStepsByDate = this.activityData.find(data => id === data.userID && date === data.date);
-    if (userStepsByDate.numSteps === userRepo.dailyStepGoal) {
-      return true;
-    }
-    return false
+    let userStepsByDate = finder(this.activityData, id, date);
+    return userStepsByDate.numSteps === userRepo.dailyStepGoal ? true : false;
   }
+
   getDaysGoalExceeded(id, userRepo) {
     return this.activityData.filter(data => id === data.userID && data.numSteps > userRepo.dailyStepGoal).map(data => data.date);
   }
+
   getStairRecord(id) {
     return this.activityData.filter(data => id === data.userID).reduce((acc, elem) => (elem.flightsOfStairs > acc) ? elem.flightsOfStairs : acc, 0);
   }
 
-  // breaks tests, but gets rid of some decimals. The values are good
   getAllUserAverageForDay(date, userRepo, relevantData) {
     let selectedDayData = userRepo.chooseDayDataForAllUsers(this.activityData, date);
-
-     // const average =
-     return parseFloat(averager(selectedDayData, relevantData).toFixed(1))
-    // return parseInt(average)
-
-     // return parseFloat((selectedDayData.reduce((acc, elem) => acc += elem[relevantData], 0) / selectedDayData.length).toFixed(1));
+     return parseFloat(averager(selectedDayData, relevantData).toFixed(1));
   }
+
   userDataForToday(id, date, userRepo, relevantData) {
     let userData = userRepo.getDataFromUserID(id, this.activityData);
     return userData.find(data => data.date === date)[relevantData];
   }
-  userDataForWeek(id, date, userRepo, releventData) {
-    return userRepo.getWeekFromDate(date, id, this.activityData).map((data) => `${data.date}: ${data[releventData]}`);
-  }
 
-  // Friends
+  userDataForWeek(id, date, userRepo, relevantData) {
+    return findDataByDate(date, id, this.activityData, userRepo, relevantData);
+  }
 
   getFriendsActivity(user, userRepo) {
     let data = this.activityData;
-    let userDatalist = user.friends.map((friend) => {
-      return userRepo.getDataFromUserID(friend, data)
-    });
-    return userDatalist.reduce((arraySoFar, listItem) => {
-      return arraySoFar.concat(listItem);
-    }, []);
+    let userDatalist = user.friends.map(friend => userRepo.getDataFromUserID(friend, data));
+    return userDatalist.reduce((arraySoFar, listItem) => arraySoFar.concat(listItem), []);
   }
 
   getFriendsAverageStepsForWeek(user, date, userRepo) {
     let friendsActivity = this.getFriendsActivity(user, userRepo);
     let timeline = userRepo.chooseWeekDataForAllUsers(friendsActivity, date);
-    return userRepo.combineRankedUserIDsAndAveragedData(friendsActivity, date, 'numSteps', timeline)
+    return userRepo.combineRankedUserIDsAndAveragedData(friendsActivity, date, 'numSteps', timeline);
   }
 
   showChallengeListAndWinner(user, date, userRepo) {
@@ -72,16 +65,15 @@ class Activity {
     return rankedList.map(listItem => {
       let userID = Object.keys(listItem)[0];
       let userName = userRepo.getDataFromID(parseInt(userID)).name;
-      return `${userName}: ${listItem[userID]}`
+      return `${userName}: ${listItem[userID]}`;
     })
   }
 
   showcaseWinner(user, date, userRepo) {
-    let namedList = this.showChallengeListAndWinner(user, date, userRepo);
-    let winner = this.showChallengeListAndWinner(user, date, userRepo).shift();
-
-    console.log('winner>>>>>>>>>>', winner);
-    return winner;
+    // NAMED LIST IS NOT BEING USED AT THIS TIME 6-6 @ 6:30PM
+    // let namedList = this.showChallengeListAndWinner(user, date, userRepo);
+    // console.log('namedList>>>>>>>>>>', namedList);
+    return this.showChallengeListAndWinner(user, date, userRepo).shift();
   }
 
   getStreak(userRepo, id, relevantData) {
@@ -100,7 +92,7 @@ class Activity {
   getWinnerId(user, date, userRepo) {
     let rankedList = this.getFriendsAverageStepsForWeek(user, date, userRepo);
     let keysList = rankedList.map(listItem => Object.keys(listItem));
-    return parseInt(keysList[0].join(''))
+    return parseInt(keysList[0].join(''));
   }
 }
 
